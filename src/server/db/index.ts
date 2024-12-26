d1b871aedb78c8ca89d50942f2048b62e2898c29
rasitechnew <rasi.tech@gmail.com>
2024-12-26 22:30:06 +0700

    update

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,4 +1,5 @@
-import { createClient, type Client } from "@libsql/client/web";
+import { createClient, type Client } from "@libsql/client";
+// import { createClient, type Client } from "@libsql/client/web";
 import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "~/env";
